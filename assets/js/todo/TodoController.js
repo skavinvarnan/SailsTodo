87de0a8e5ebda4b2a25bb3dd71a576cd1b768e3c
Kavin Varnan <skavinvarnan@gmail.com>
2016-02-10 17:11:48 +0530

    HTTP request for adding a task

--- assets/js/todo/TodoController.js
+++ assets/js/todo/TodoController.js
@@ -1,5 +1,6 @@
-angular.module('TodoModule').controller('TodoController', ['$scope', function($scope) { 
+angular.module('TodoModule').controller('TodoController', ['$scope', '$http', '$mdDialog', function($scope, $http, $mdDialog) { 
   $scope.helloAngular = "Hello World Angular";
+  $scope.showProgressbar = false;
   
   $scope.todos = [
       {
@@ -23,5 +24,39 @@ angular.module('TodoModule').controller('TodoController', ['$scope', function($s
         desc: 'Create the quiz list screen from scratch'
       }
     ];
+    
+  $scope.addTask = function() {
+    $scope.showProgressbar = true;
+    $scope.task.status = false;
+    $http.post("/api/add_task", $scope.task)
+    .then(
+      function success(response) {
+        $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(false)
+          .title('Task Added')
+          .textContent('Your task has been successfully added.')
+          .ariaLabel('Task Added')
+          .ok('ok')
+        ).then(function() {
+          $scope.task.name = '';
+          $scope.task.description = '';
+        });
+      }, 
+      function error(response) {
+        $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(false)
+          .title('Error')
+          .textContent('Error while adding task. Please try again.')
+          .ariaLabel('Error')
+          .ok('ok')
+        );
+    }).finally(function() {
+      $scope.showProgressbar = false;
+    });
+  }
   
 }]);
