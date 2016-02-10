277a9ed260564a5742211b280112806306d25c4f
Kavin Varnan <skavinvarnan@gmail.com>
2016-02-10 16:31:59 +0530

    Adding dummy TODO list

--- assets/js/todo/TodoController.js
+++ assets/js/todo/TodoController.js
@@ -1,4 +1,27 @@
 angular.module('TodoModule').controller('TodoController', ['$scope', function($scope) { 
   $scope.helloAngular = "Hello World Angular";
   
+  $scope.todos = [
+      {
+        name: 'Achiever Dashboard UI items',
+        desc: 'Replicate the same design from iOS to Android'
+      },
+      {
+        name: 'Send message through notification',
+        desc: 'Notification should be received on a new message arrival'
+      },
+      {
+        name: 'Crash on dialog display',
+        desc: 'Dialog box gets crashed when screen idle for a long time'
+      },
+      {
+        name: 'Hide keyboard in edit profile screen',
+        desc: 'Hide the keyboard on the tap of the background image. Refer iOS'
+      },
+      {
+        name: 'Quiz list screen',
+        desc: 'Create the quiz list screen from scratch'
+      }
+    ];
+  
 }]);
