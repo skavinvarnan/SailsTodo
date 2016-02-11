9c4589e24da9014fb35a13328ed908b598d1878e
Kavin Varnan <skavinvarnan@gmail.com>
2016-02-11 18:21:09 +0530

    Removing min from JS and CSS. Let grunt handle this task

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -34,16 +34,16 @@ module.exports = function(grunt) {
 		},
     jsDeps: {
       files: [{
-        '.tmp/public/js/dependencies/angular.js': 'bower_components/angular/angular.min.js',
-        '.tmp/public/js/dependencies/angular-animate.js': 'bower_components/angular-animate/angular-animate.min.js',
-        '.tmp/public/js/dependencies/angular-aria.js': 'bower_components/angular-aria/angular-aria.min.js',
-        '.tmp/public/js/dependencies/angular-messages.js': 'bower_components/angular-messages/angular-messages.min.js',
-        '.tmp/public/js/dependencies/angular-material.js': 'bower_components/angular-material/angular-material.min.js'
+        '.tmp/public/js/dependencies/angular.js': 'bower_components/angular/angular.js',
+        '.tmp/public/js/dependencies/angular-animate.js': 'bower_components/angular-animate/angular-animate.js',
+        '.tmp/public/js/dependencies/angular-aria.js': 'bower_components/angular-aria/angular-aria.js',
+        '.tmp/public/js/dependencies/angular-messages.js': 'bower_components/angular-messages/angular-messages.js',
+        '.tmp/public/js/dependencies/angular-material.js': 'bower_components/angular-material/angular-material.js'
       }]
     },
     cssDeps: {
       files: [{
-        '.tmp/public/styles/angular-material/angular-material.css': 'bower_components/angular-material/angular-material.min.css'
+        '.tmp/public/styles/angular-material/angular-material.css': 'bower_components/angular-material/angular-material.css'
       }]
     }
 	});
